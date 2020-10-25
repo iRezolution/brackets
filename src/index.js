b605499d866884bdc844653ebccf4e7363772018
Sergey Voytenko <open_your_eyes_res@mail.ru>
2020-10-25 23:03:14 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let map = new Map();
+    let stack = [];
+
+    for (brackets of bracketsConfig) {
+        map.set(brackets[0], brackets[1]);
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === stack[stack.length - 1]) {
+            stack.pop();
+        } else {
+            stack.push(map.get(str[i]));
+        }
+    }
+
+    return stack.length === 0 ? true : false;
+}
 }
